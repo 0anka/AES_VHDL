2235399b4a787030b64846ef3f8079fc96886551
malefax <shiven522002@gmail.com>
2026-08-13 14:04:54 +0530

    BYTE_SEQUENCES

---
+++ pkg/Byte_sequences.vhd
@@ -0,0 +1,19 @@
+-- IEEE 1076 and IEEE 1164
+-- IEEE 1076.6 DISCOURAGE TILL NOW
+-- PRODUCTION RULE FOLLOWED BY BNF GRAMMER AS PER IEEE 1076
+--  Copyright (C) 2026 Malefax
+-- GENERAL SEQUENCE OF BITS
+--r0 r1 r2 . . . r(8k−3) r(8k−2) r(8k−1) FOR 8K BITS
+-- FOR 128 BITS => r0 r1 r2 . . . r125 r126 r127 
+--IMPLEMENTATION r127 r126 r125 ... r2 r1 r0
+library IEEE; use IEEE.STD_LOGIC_1164.ALL;
+package BYTE_SEQUENCES is
+        type S_IND is range 15 downto 0;
+        type W_IND is range 3 downto 0;
+        subtype AES_128 is std_logic_vector(127 downto 0 );
+        subtype \8bit\ is std_logic_vector ( 7 downto 0 );
+        subtype \32bit\ is std_logic_vector (31 downto 0);
+        type STATE is array ( S_IND range <> ) of \8bit\;
+        type WORD is array ( W_IND range <> ) of \32bit\;
+end package BYTE_SEQUENCES;        
+
