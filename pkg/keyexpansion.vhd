7f18349f85742bfa298a94fc94e8600f3dbebd78
malefax <shiven522002@gmail.com>
2026-09-06 23:30:25 +0530

    fix compilation error due to case sesitive name bnf convention, typecaste coun

--- pkg/keyexpansion.vhd
+++ pkg/keyexpansion.vhd
@@ -12,18 +12,18 @@ package KEYEXPANSION is
         KEY_DONE
     );
         subtype counter is integer range 0 to 10;
-        function key_expansion(word:WORD(3 downto 0);coun:counter) return WORD; 
+        function key_expansion(\word\:WORD(3 downto 0);coun:counter) return WORD; 
 end package KEYEXPANSION;
 package body KEYEXPANSION is
-        function key_expansion(word:WORD(3 downto 0);coun:counter) return WORD is
+        function key_expansion(\word\:WORD(3 downto 0);coun:counter) return WORD is
                 variable word_out:WORD(3 downto 0) := (others=>(others=>'0'));
                 variable temp:\32bit\;
         begin
-                temp := subword(rotword(word(0))) xor Rcon(coun);
-                word_out(3) := word(3) xor temp;
-                word_out(2) := word(2) xor word_out(3);
-                word_out(1) := word(1) xor word_out(2);
-                word_out(0) := word(0) xor word_out(1);
+                temp := subword(rotword(\word\(0))) xor \Rcon\(S_IND(coun));
+                word_out(3) := \word\(3) xor temp;
+                word_out(2) := \word\(2) xor word_out(3);
+                word_out(1) := \word\(1) xor word_out(2);
+                word_out(0) := \word\(0) xor word_out(1);
                 return word_out;
         end function key_expansion;
 end package body;
