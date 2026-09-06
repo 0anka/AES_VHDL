ae5fd6c451d40613c9d3d308258fd5b6de53663a
malefax <shiven522002@gmail.com>
2026-09-06 23:10:07 +0530

    fix compilation error due to srl on type enum change object to unsigned

--- pkg/gf_mul.vhd
+++ pkg/gf_mul.vhd
@@ -3,7 +3,7 @@
 -- IEEE 1076.6 DISCOURAGE TILL NOW
 -- PRODUCTION RULE FOLLOWED BY BNF GRAMMER AS PER IEEE 1076
 --  Copyright (C) 2026 Malefax
-library IEEE; use IEEE.STD_LOGIC_1164.ALL;
+library IEEE; use IEEE.STD_LOGIC_1164.ALL;use ieee.numeric_std.all;
 package RUSSIAN_PEASENT is
         function gf_mul(\8BIT1\:std_logic_vector(7 downto 0);\8BIT2\:std_logic_vector(7 downto 0)) return std_logic_vector;
         constant  \0x01\:std_logic_vector(7 downto 0) := ( 7 downto 1 => '0',others => '1');
@@ -23,11 +23,11 @@ package  body RUSSIAN_PEASENT is
                            RP_VAL:= RP_VAL xor \8BIT_VAL1\;
                     end if AND_COND;
                 HIGH_BIT:= \8BIT_VAL1\(7) ;
-                \8BIT_VAL1\ := \8BIT_VAL1\ sll 1;
+                \8BIT_VAL1\ := std_logic_vector(unsigned(\8BIT_VAL1\) sll 1);
                 XOR_COND:if HIGH_BIT = '1' then
                         \8BIT_VAL1\ := \8BIT_VAL1\ xor \0x1b\;
                     end if XOR_COND;
-                \8BIT_VAL2\ := \8BIT_VAL2\ srl 1;
+                \8BIT_VAL2\ := std_logic_vector(unsigned(\8BIT_VAL2\) srl 1);
         end loop loop8times;
         return RP_VAL;
 end function gf_mul;
